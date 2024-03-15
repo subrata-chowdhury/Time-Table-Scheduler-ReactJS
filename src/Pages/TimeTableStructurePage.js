40998572a8629fe2cda9471a8ab6d965f32d467f
Subrata Chowdhury <subratachowdhury7000@gmail.com>
2024-03-15 20:49:03 +0530

    fixed a bug

--- src/Pages/TimeTableStructurePage.js
+++ src/Pages/TimeTableStructurePage.js
@@ -36,7 +36,7 @@ function TimeTableStructureInputContainer({ fileChange, setFileChange }) {
         event.preventDefault();
         let newTimeTableStructure = { ...timeTableStructure };
         try {
-            newTimeTableStructure["breaksPerSemester"] = JSON.parse(`[${timeTableStructure["breaksPerSemester"]}]`);
+            newTimeTableStructure["breaksPerSemester"] = JSON.parse(`[${JSON.stringify(timeTableStructure["breaksPerSemester"]).slice(1,-1)}]`);
             newTimeTableStructure["sectionsPerSemester"] = JSON.parse(`[${timeTableStructure["sectionsPerSemester"]}]`);
         } catch (err) {
             alert("Please Enter the values correctly")
