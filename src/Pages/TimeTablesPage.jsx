abad960cf6cde3ae52dc7c916a20a3fd5d26089c
Subrata Chowdhury <subratachowdhury7000@gmail.com>
2024-03-29 20:03:13 +0530

    fixed an error

--- src/Pages/TimeTablesPage.jsx
+++ src/Pages/TimeTablesPage.jsx
@@ -3,7 +3,7 @@ import Menubar from '../Components/Menubar'
 import TimeTable from '../Components/TimeTable'
 import "../Style/TimeTablesPage.css"
 import Cards, { Card, HorizentalCardsContainer } from '../Components/Cards'
-import { useCallback, useEffect, useRef, useState } from 'react'
+import { memo, useCallback, useEffect, useRef, useState } from 'react'
 import { getSubjects } from '../Script/SubjectsDataFetcher'
 import { generateTimeTable, getSchedule, getTimeTableStructure, saveSchedule } from '../Script/TimeTableDataFetcher'
 import { getTeacherList } from '../Script/TeachersDataFetcher'
